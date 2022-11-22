6d24ea8c4c0abe832a060e553568b3aeb15fc892
Andrey <antonov.andrey.10101@gmail.com>
2022-11-22 16:07:36 +0300

    feat: add logic for checking correctness of answer

--- src/components/QuizCard/QuizCars.tsx
+++ src/components/QuizCard/QuizCars.tsx
@@ -1,5 +1,5 @@
 import { Button, Flex, Heading } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import QuizApi from "../../services/QuizApi";
 import { IQuiz } from "../../types/quiz.type";
@@ -8,13 +8,31 @@ function QuizCard() {
   const { getRandomQuiz } = QuizApi();
   const [quiz, setQuiz] = useState<IQuiz>();
 
+  const buttonClick = (e: BaseSyntheticEvent) => {
+    if (quiz) {
+      const answers = Object.entries(quiz.answers);
+      const correctAnswers = Object.entries(quiz.correct_answers);
+      const findEl = answers.find((item) => item[1] === e.target.innerText);
+
+      if (findEl) {
+        for (const key in correctAnswers) {
+          const correctAnswer = correctAnswers[key];
+          if (
+            correctAnswer[0].includes(findEl[0]) &&
+            correctAnswer[1] === "true"
+          ) {
+            console.log("NICE!");
+          }
+        }
+      }
+    }
+  };
+
   useEffect(() => {
     getRandomQuiz()
       .then((data) => setQuiz(...data))
       .catch((error) => console.log(error));
   }, []);
-
-  console.log(quiz);
 
   return (
     <Flex
@@ -22,7 +40,6 @@ function QuizCard() {
       p="10"
       flexDirection="column"
       alignItems="center"
-      maxW="900"
     >
       {quiz ? (
         <>
@@ -36,7 +53,13 @@ function QuizCard() {
             wrap="wrap"
           >
             {Object.entries(quiz.answers).map((item) =>
-              item[1] !== null ? <Button key={uuidv4()}>{item[1]}</Button> : ""
+              item[1] !== null ? (
+                <Button key={uuidv4()} onClick={buttonClick}>
+                  {item[1]}
+                </Button>
+              ) : (
+                ""
+              )
             )}
           </Flex>
         </>
--- src/services/QuizApi.ts
+++ src/services/QuizApi.ts
@@ -7,6 +7,11 @@ export default function QuizApi() {
   const getRandomQuiz = async () => {
     const res = await fetch(`${apiUrl}?apiKey=${API_KEY}&limit=1`);
     const data: IQuiz[] = await res.json();
+    //
+    // const res2 = await fetch("https://opentdb.com/api.php?amount=10");
+    // const data2: IQuiz[] = await res2.json();
+    // console.log(data2);
+
     return data;
   };
 
