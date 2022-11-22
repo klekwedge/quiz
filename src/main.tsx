76b64334910d9ef88c71f2d0e74159469fddca64
Andrey <antonov.andrey.10101@gmail.com>
2022-11-22 13:34:41 +0300

    feat: add basic project structure

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import App from './components/App/App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ChakraProvider>
+    <App />
+    </ChakraProvider>
+)
